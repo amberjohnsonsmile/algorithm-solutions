0de48383b1d3d31c8923573c69d4f72e9eaf5e40
Amber Johnson <amberjohnsonsmile@gmail.com>
2025-05-01 11:56:34 -0600

    Working on solution for merge meeting times

--- src/arrays-and-strings/01-merging-meeting-times.js
+++ src/arrays-and-strings/01-merging-meeting-times.js
@@ -1,14 +1,34 @@
 // Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns an array of condensed ranges
 // Start time and end time don't have an upper bound
 export function mergeRanges(ranges) {
-  let condensedRanges = []
+  let condensedRanges = ranges[0]
+
   // TODO implement custom method for sorting
   const sortedRanges = ranges.sort((a, b) => a.startTime - b.startTime)
   console.log('sorted: ', sortedRanges)
 
   // Two at a time, see if there's overlap
-    // If there's overlap, combine the two then add it to solution []
-    // If not add both
+  // Compare current item to last item in solution
+  for (let i = 1; i < ranges.length; i++) {
+    const last = sortedRanges[sortedRanges.length - 1]
+    const current = ranges[i]
+
+    // Instead of comparing current and next, add first to solution then compare current to last added to solution
+    // If there's overlap
+    if (last.endTime < current.startTime) {
+      condensedRanges.push({
+        startTime: current.startTime,
+        // Do I need this max?
+        endTime: Math.max(current.endTime, next.endTime)
+      })
+    } else {
+      // If not add both
+      condensedRanges.push(current)
+    }
+    // What if three in a row overlap?
+    // What if one is completely within the other?
+  }
+  console.log('condensed ranges are: ', condensedRanges)
   return condensedRanges
 }
 
@@ -19,6 +39,15 @@ export function mergeRanges(ranges) {
 //   { startTime: 4,  endTime: 8 },
 //   { startTime: 10, endTime: 12 },
 //   { startTime: 9,  endTime: 10 },
+// ]
+
+// Sorted:
+// [
+//   { startTime: 0,  endTime: 1 },
+//   { startTime: 3,  endTime: 5 },
+//   { startTime: 4,  endTime: 8 },
+//   { startTime: 9,  endTime: 10 },
+//   { startTime: 10, endTime: 12 },
 // ]
 
 // Output:
--- test/arrays-and-strings/01-merging-meeting-times.test.js
+++ test/arrays-and-strings/01-merging-meeting-times.test.js
@@ -2,23 +2,23 @@ import { mergeRanges } from '../../src/arrays-and-strings/01-merging-meeting-tim
 import * as assert from 'assert'
 
 describe('mergeRanges', function () {
-  it('should return an array of condensed meeting time ranges', () => {
-    const input = [
-      { startTime: 0, endTime: 1 },
-      { startTime: 3, endTime: 5 },
-      { startTime: 4, endTime: 8 },
-      { startTime: 10, endTime: 12 },
-      { startTime: 9, endTime: 10 }
-    ]
-    const expected = [
-      { startTime: 0, endTime: 1 },
-      { startTime: 3, endTime: 8 },
-      { startTime: 9, endTime: 12 }
-    ]
+  // it('should return an array of condensed meeting time ranges', () => {
+  //   const input = [
+  //     { startTime: 0, endTime: 1 },
+  //     { startTime: 3, endTime: 5 },
+  //     { startTime: 4, endTime: 8 },
+  //     { startTime: 10, endTime: 12 },
+  //     { startTime: 9, endTime: 10 }
+  //   ]
+  //   const expected = [
+  //     { startTime: 0, endTime: 1 },
+  //     { startTime: 3, endTime: 8 },
+  //     { startTime: 9, endTime: 12 }
+  //   ]
 
-    const result = mergeRanges(input)
-    assert.deepEqual(result, expected)
-  })
+  //   const result = mergeRanges(input)
+  //   assert.deepEqual(result, expected)
+  // })
 
   //   it('should merge adjacent meetings', () => {
   //     const input = [
@@ -42,16 +42,16 @@ describe('mergeRanges', function () {
   //     assert.deepEqual(result, expected)
   //   })
 
-  //   it('should merge lots of meetings into one', () => {
-  //     const input = [
-  //       { startTime: 1, endTime: 10 },
-  //       { startTime: 2, endTime: 6 },
-  //       { startTime: 3, endTime: 5 },
-  //       { startTime: 7, endTime: 9 }
-  //     ]
-  //     const expected = [{ startTime: 1, endTime: 10 }]
+  it('should merge lots of meetings into one', () => {
+    const input = [
+      { startTime: 1, endTime: 10 },
+      { startTime: 2, endTime: 6 },
+      { startTime: 3, endTime: 5 },
+      { startTime: 7, endTime: 9 }
+    ]
+    const expected = [{ startTime: 1, endTime: 10 }]
 
-  //     const result = mergeRanges(input)
-  //     assert.deepEqual(result, expected)
-  //   })
+    const result = mergeRanges(input)
+    assert.deepEqual(result, expected)
+  })
 })

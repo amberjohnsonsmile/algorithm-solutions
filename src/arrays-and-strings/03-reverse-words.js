9f4b740ceda6ff1bc66606aecaefaa3df5400a91
Amber Johnson <amberjohnsonsmile@gmail.com>
2025-05-09 13:24:24 -0600

    Finish coding reverse words

--- src/arrays-and-strings/03-reverse-words.js
+++ src/arrays-and-strings/03-reverse-words.js
@@ -1,35 +1,26 @@
+// Write a function reverseWords() that takes a message as an array of characters and reverses the order of the words in place.
 export function reverseWords(charArr) {
   // Reverse all
-  let i = 0
-  let j = charArr.length - 1
-  while (i < j) {
-    const temp = charArr[i]
-    charArr[i] = charArr[j]
-    charArr[j] = temp
-    i++
-    j--
-  }
-  console.log('after reversing all: ', charArr)
+  reverseCharacters(charArr, 0, charArr.length - 1)
+
   // Reverse one word at a time
-
-  let placeholder = 0
-  i = 0
-  j = 0
-
-  while (placeholder < charArr.length) {
-    while (charArr[j] !== ' ' && j < charArr.length) {
-      j++
-    }
-
-    placeholder = j + 2
-
-    while (i < j) {
-      const temp = charArr[i]
-      charArr[i] = charArr[j]
-      charArr[j] = temp
-      i++
-      j--
+  let wordStart = 0
+  for (let i = 0; i <= charArr.length; i++) {
+    if (i === charArr.length || charArr[i] === ' ') {
+      reverseCharacters(charArr, wordStart, i - 1)
+      wordStart = i + 1
     }
   }
+
   return charArr
+}
+
+function reverseCharacters(charArr, leftIndex, rightIndex) {
+  while (leftIndex < rightIndex) {
+    const temp = charArr[leftIndex]
+    charArr[leftIndex] = charArr[rightIndex]
+    charArr[rightIndex] = temp
+    leftIndex++
+    rightIndex--
+  }
 }

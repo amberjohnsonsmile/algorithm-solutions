999ddf0a08f063d06c434aa1f8620ac51eacc19c
Amber Johnson <amberjohnsonsmile@gmail.com>
2025-05-30 13:18:33 -0600

    Add tests for cafe order checker

--- src/arrays-and-strings/05-cafe-order-checker.js
+++ src/arrays-and-strings/05-cafe-order-checker.js
@@ -21,34 +21,3 @@ export function checkOrders(takeOutOrders, dineInOrders, servedOrders) {
 
   return true
 }
-
-// Take Out Orders: [1, 3, 5]
-// Dine In Orders: [2, 4, 6]
-// Served Orders: [1, 2, 4, 6, 5, 3]
-// console.log(checkOrders([1, 3, 5], [2, 4, 6], [1, 2, 4, 6, 5, 3]));
-// false
-
-// Take Out Orders: [17, 8, 24]
-// Dine In Orders: [12, 19, 2]
-// Served Orders: [17, 8, 12, 19, 24, 2]
-// console.log(checkOrders([17, 8, 24], [12, 19, 2], [17, 8, 12, 19, 24, 2]));
-// true
-
-// [5, 2]
-// [7, 3]
-// true [5, 7, 2, 3]
-// false [2, 5, 7, 3]
-
-// console.log(checkOrders([5, 2], [7, 3], [2, 5, 7, 3]))
-// false
-
-// console.log(checkOrders([5, 2], [7, 3], [5, 7, 2, 3]))
-// true
-
-// [2]
-// [1, 4]
-// console.log(checkOrders([2], [1, 4], [1, 2, 4]))
-// true
-
-console.log(checkOrders([2], [1, 4], [4, 2, 1]))
-// false
--- test/arrays-and-strings/05-cafe-order-checker.test.js
+++ test/arrays-and-strings/05-cafe-order-checker.test.js
@@ -7,6 +7,62 @@ describe('checkOrders', function () {
     const dineInOrders = [2, 4, 6]
     const servedOrders = [1, 2, 4, 6, 5, 3]
     const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
-    assert.deepEqual(result, false)
+    assert.equal(result, false)
+  })
+
+  it('should validate a correct order', () => {
+    const takeOutOrders = [1, 3, 5]
+    const dineInOrders = [2, 4, 6]
+    const servedOrders = [1, 2, 4, 3, 5, 6]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, true)
+  })
+
+  it('should handle an empty array of dine in orders', () => {
+    const takeOutOrders = [1, 3, 5]
+    const dineInOrders = []
+    const servedOrders = [1, 3, 5]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, true)
+  })
+
+  it('should handle an empty array of take out orders', () => {
+    const takeOutOrders = []
+    const dineInOrders = [2, 4, 6]
+    const servedOrders = [2, 4, 6]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, true)
+  })
+
+  it('should validate when order numbers that are not in order', () => {
+    const takeOutOrders = [8, 2, 5]
+    const dineInOrders = [3, 4, 1]
+    const servedOrders = [8, 3, 4, 2, 5, 1]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, true)
+  })
+
+  it('should identify incorrect order with numbers that are not in order', () => {
+    const takeOutOrders = [8, 2, 5]
+    const dineInOrders = [3, 4, 1]
+    const servedOrders = [8, 5, 4, 2, 3, 1]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, false)
+  })
+
+  it('should identify incorrect order with arrays of different sizes', () => {
+    const takeOutOrders = [1, 3, 5, 7]
+    const dineInOrders = [2, 4, 6]
+    const servedOrders = [1, 2, 4, 6, 5, 3, 7]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, false)
+  })
+
+  it('should validate correct order with arrays of different sizes', () => {
+    const takeOutOrders = [1, 3, 5, 7]
+    const dineInOrders = [2, 4, 6]
+    const servedOrders = [1, 2, 4, 6, 3, 5, 7]
+    const result = checkOrders(takeOutOrders, dineInOrders, servedOrders)
+    assert.equal(result, true)
   })
 })

a4d2353542ca4a484838140e843c1878efb62c18
Amber Johnson <amberjohnsonsmile@gmail.com>
2025-06-30 12:31:42 -0600

    Simplify inflight entertainment problem by only looping once

--- src/hashing-and-hash-tables/06-inflight-entertainment.js
+++ src/hashing-and-hash-tables/06-inflight-entertainment.js
@@ -1,20 +1,17 @@
 // Write a function that takes an integer flightLength (in minutes) and an array of integers movieLengths (in minutes) and returns a boolean indicating whether there are two numbers in movieLengths whose sum equals flightLength.
 export function isPossibleToFillFlightTime(flightLength, movieLengths) {
   let movieTracker = {}
+  let isPossible = false
 
-  // First, add as object keys
+  // Add keys to map and check for difference
   movieLengths.forEach((movieLength) => {
+    const difference = flightLength - movieLength
+    if (movieTracker[difference]) {
+      isPossible = true
+    }
+
     movieTracker[movieLength] = true
   })
 
-  // Next, compare time difference against existing keys
-  for (let i = 0; i < movieLengths.length; i++) {
-    const difference = flightLength - movieLengths[i]
-    if (movieTracker[difference] && difference !== movieLengths[i]) {
-      return true
-    }
-  }
-
-  // If nothing returns true, we know it can't be done
-  return false
+  return isPossible
 }
